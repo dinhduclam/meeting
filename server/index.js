77d32618c20a4cd0d38d9fa7786a0ad185291de9
Duc Lam <dinhduclam2001@gmail.com>
2024-02-02 03:20:59 +0700

    update server socket

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const socket = require('socket.io');
 const initSocket = require('./lib/socket');
 const httpServer = http.createServer();
 const io = new socket.Server(httpServer, {
-  path: '/bridge'
+  path: '/bridge',
+  cors: {
+    origin: ["http://localhost:3000", "*"],
+  },
 });
 
 const port = 5000;
